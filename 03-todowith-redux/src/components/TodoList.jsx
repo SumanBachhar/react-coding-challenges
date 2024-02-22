4259073ce123b66d01bb61411e8acfa42c02d166
Suman-199 <suman.mmt199@gmail.com>
2024-02-22 18:06:38 +0530

    todolist completed

--- 03-todowith-redux/src/components/TodoList.jsx
+++ 03-todowith-redux/src/components/TodoList.jsx
@@ -1,3 +1,60 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, removeTodo, editTask } from "../features/todoSlice";
+
 export const TodoList = () => {
-  return <>App</>;
+  const todos = useSelector((state) => state.todos);
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+  const [editId, setEditId] = useState(null);
+
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    if (text.trim() === "") return;
+    if (editId !== null) {
+      dispatch(editTask({ id: editId, text }));
+      setEditId(null);
+    } else {
+      dispatch(addTodo(text));
+    }
+    setText("");
+  };
+
+  const handleEditTask = (id, text) => {
+    setEditId(id);
+    setText(text);
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={addTodoHandler}>
+          <h2>Todo List</h2>
+          <input
+            type="text"
+            placeholder="Enter a Todo..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button type="submit">{editId !== null ? "Update" : "Add"}</button>
+        </form>
+      </div>
+
+      <div>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.text}
+              <button type="text" onClick={() => dispatch(removeTodo(todo.id))}>
+                Delete
+              </button>
+              <button onClick={() => handleEditTask(todo.id, todo.text)}>
+                Edit
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
--- 03-todowith-redux/src/features/todoSlice.js
+++ 03-todowith-redux/src/features/todoSlice.js
@@ -3,22 +3,33 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 const initialState = {
   todos: [{ id: 1, text: "Hello world" }],
 };
-
 export const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
     // Add todo
-    addtodo: (state, actions) => {
+    addTodo: (state, action) => {
       const todo = {
         id: nanoid(),
-        text: actions.payload,
+        text: action.payload,
       };
       state.todos.push(todo);
     },
-    // remove todo
+    // Remove todo
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    // Update todo
+    editTask: (state, action) => {
+      const { id, text } = action.payload;
+      const taskIndex = state.todos.findIndex((todo) => todo.id === id);
+      if (taskIndex !== -1) {
+        state.todos[taskIndex].text = text;
+      }
+    },
   },
 });
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, editTask } = todoSlice.actions;
+
 export default todoSlice.reducer;
